59e423ee4ff17928752b0cdcd46b9a08b12581fb
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-09-19 21:50:45 +0100

    jigsaw finished

--- src/app/Project.ts
+++ src/app/Project.ts
@@ -17,6 +17,10 @@ export interface Project {
     screenshot3Description?: string;
     screenshot4?: string;
     screenshot4Description?: string;
+    screenshot5?: string;
+    screenshot5Description?: string;
+    screenshot6?: string;
+    screenshot6Description?: string;
     code1?: string;
     code1Description?: string;
     code2?: string;
@@ -27,4 +31,14 @@ export interface Project {
     code4Description?: string;
     code5?: string;
     code5Description?: string;
+    code6?: string;
+    code6Description?: string;
+    code7?: string;
+    code7Description?: string;
+    code8?: string;
+    code8Description?: string;
+    code9?: string;
+    code9Description?: string;
+    code10?: string;
+    code10Description?: string;
 }
